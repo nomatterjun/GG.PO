ddb91e301d8918412f59f5644a6ce1e87fd2577b
junstone1995 <boris0730@gmail.com>
2023-06-10 14:15:15 +0900

    model 추가

---
+++ GGPO/Model/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  GGPO
+//
+//  Created by Lee on 2023/06/10.
+//
+
+import Foundation
+import SwiftData
+
+
+final class User {
+    var name: String
+    var tier: String
+    var point: Int
+    var mostChapion: Champion?
+    
+    init(name: String, tier: String, point: Int, mostChapion: Champion? = nil) {
+        self.name = name
+        self.tier = tier
+        self.point = point
+        self.mostChapion = mostChapion
+    }
+}
